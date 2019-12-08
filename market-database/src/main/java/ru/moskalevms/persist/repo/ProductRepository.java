111f374c49f5c483e034b49f98bb384ea7782a74
moskalevms <mis-van@yandex.ru>
2019-12-08 17:35:08 +0300

    ДЗ по пятому уроку

--- market-database/src/main/java/ru/moskalevms/persist/repo/ProductRepository.java
+++ market-database/src/main/java/ru/moskalevms/persist/repo/ProductRepository.java
@@ -1,14 +1,7 @@
 package ru.moskalevms.persist.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.moskalevms.persist.model.Product;
-
-import javax.validation.constraints.Max;
-import java.awt.print.Pageable;
-import java.util.List;
-import java.util.Optional;
 
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
