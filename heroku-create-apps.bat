cd9067881dbd21908441d3386c93f0465f455570
moskalevms <mis-van@yandex.ru>
2019-12-15 12:35:33 +0300

    ДЗ по деплою

---
+++ heroku-create-apps.bat
@@ -0,0 +1,3 @@
+heroku apps:create moskalems-market-admin-ui
+heroku addons:create heroku-postgresql:hobby-dev --app moskalevms-market-admin-ui
+heroku config:set JDBC_DRIVER_CLASS=org.postgresql.Driver HIBERNATE_DIALECT=org.hibernate.dialect.PostgreSQLDialect --app moskalevms-market-admin-ui
